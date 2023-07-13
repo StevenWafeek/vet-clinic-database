86ba5cb2a95c464f4671b652b2c676a852e55938
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-07-13 10:32:20 +0300

    Update schema file

--- schema.sql
+++ schema.sql
@@ -1,20 +1,19 @@
-/* Database schema to keep the structure of entire database. */
-
+ /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE owners (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id SERIAL PRIMARY KEY,
   full_name VARCHAR(255),
   age INTEGER
 );
 
 CREATE TABLE species (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255)
 );
 
 CREATE TABLE animals (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(55),
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
   date_of_birth DATE,
   escape_attempts INTEGER,
   neutered BOOLEAN,
@@ -23,4 +22,24 @@ CREATE TABLE animals (
   owner_id INTEGER,
   FOREIGN KEY (species_id) REFERENCES species(id),
   FOREIGN KEY (owner_id) REFERENCES owners(id)
+);
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+  vet_id INTEGER REFERENCES vets(id),
+  species_id INTEGER REFERENCES species(id),
+  PRIMARY KEY (vet_id, species_id)
+);
+
+CREATE TABLE visits (
+  id SERIAL PRIMARY KEY,
+  animal_id INTEGER REFERENCES animals(id),
+  vet_id INTEGER REFERENCES vets(id),
+  visit_date DATE
 );
